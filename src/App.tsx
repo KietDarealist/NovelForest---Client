6ada793171214da796559b3ff3af87ef8eec1727
KietDarealist <kietmakietna@gmail.com>
2021-12-24 06:27:04 +0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+
 
 function App() {
   return (
     <div className="App">
-        <h3 className="text-red-500">Hello World</h3>
+       
     </div>
   );
 }
