c2dafb5589a0de2a560cfec7508219225bb4775b
KietDarealist <kietmakietna@gmail.com>
2021-12-25 10:40:27 +0700

    delete redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppWrapper from './App';
-import {Provider} from "react-redux"
-import {store} from "./states/index";
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
     <AppWrapper />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/states/action-types/index.ts
+++
@@ -1,4 +0,0 @@
-export enum UserActionType{
-    SET_AUTH = "set_auth",
-    UPDATE_USER = "update_user"
-}
